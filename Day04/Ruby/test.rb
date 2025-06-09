cd2994cc601d712c807c1f71e50f099f8e57c2fb
Benjamin Flanders <flanderb@gmail.com>
2025-06-09 19:30:01 +0000

    Day 4a is done

--- Day04/Ruby/test.rb
+++ Day04/Ruby/test.rb
@@ -4,25 +4,25 @@ require_relative 'Day04a'
 class Day03Test < Minitest::Test
 
     def setup
-        @word_search = Day04.new("XMAS","../1_test.txt")
+        @word_search = Day04.new("XMAS","../test_input.txt")
         # @word_search = Day04.new("XMAS","../test_input.txt")
     end
 
-    # def test_file_is_there
-    #     assert @word_search.file != nil
-    # end 
+    def test_file_is_there
+        assert @word_search.file != nil
+    end 
 
-    # def test_ws_matrix
-    #     assert @word_search.word_search_matrix.instance_of?(Matrix)
-    # end
+    def test_ws_matrix
+        assert @word_search.word_search_matrix.instance_of?(Matrix)
+    end
 
-    # def test_matrix_search_false
-    #     assert @word_search.number_of_words_at_index('XMAS', 0, 0, @word_search.word_search_matrix) == 0
-    # end
+    def test_matrix_search_false
+        assert @word_search.number_of_words_at_index('XMAS', 0, 0, @word_search.word_search_matrix) == 0
+    end
 
-    # def test_matrix_search_true
-    #     assert @word_search.number_of_words_at_index('XMAS', 0, 4, @word_search.word_search_matrix) == 1
-    # end
+    def test_matrix_search_true
+        assert @word_search.number_of_words_at_index('XMAS', 0, 4, @word_search.word_search_matrix) == 1
+    end
 
     def test_count_all_words
         # assert @word_search.number_of_words_found == 18
